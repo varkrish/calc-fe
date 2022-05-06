cc20f11a6354986c5fcc22a19b4939b5dc8d2376
varkrish <--get-all>
2022-05-06 16:08:05 +1000

    Fixing form issues and enhancing mock

--- assets/callbackend.js
+++ assets/callbackend.js
@@ -1,11 +1,10 @@
 var backendUrl="http://localhost:8080/assets/test/add.json"
-
+var tableCounter=1;
 function callBackEnd()
 {
  var operand1= $('#operand_1').val(); 
  var operand2= $('#operand_2').val(); 
  var operator= $('#operator :selected').text();
- alert('calling backend' + operator);
  var posting = $.get(backendUrl, {
     "operand1": operand1,
     "operand2": operand2,
@@ -15,8 +14,22 @@ function callBackEnd()
     /* Alerts the results */
     posting.done(function(data) {
         $('#result').text('Result is ' + data['result']);
+        addResultToTable(data);
       });
       posting.fail(function() {
         $('#result').text('failed');
       });
+}
+
+function addResultToTable(data)
+{
+  var responses = $('#calc_history').DataTable();
+  responses.row.add( [
+          tableCounter++,
+          data['input']['operand_1'],
+          data['input']['operand_2'],
+          data['input']['operator'],
+          data.result,
+          "Unknown"
+      ] ).draw( true );
 }
